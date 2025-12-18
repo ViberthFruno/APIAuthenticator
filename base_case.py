153ac7bc46b9cea8da615f442f71145e0cb8bb22
Viberth Wingrove <97918728+YaushuDev@users.noreply.github.com>
2025-12-18 10:04:56 -0600

    ajustes de texto

--- base_case.py
+++ base_case.py
@@ -1,5 +1,5 @@
 # Archivo: base_case.py
-# Ubicación: raíz del proyecto
+# Ubicación: raíz del proyecto.
 # Descripción: Clase base para todos los casos de procesamiento de correo.
 
 from config_manager import ConfigManager
